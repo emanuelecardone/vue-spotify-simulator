3767fb59362d468533051e709373e9030da8350c
emanuelecardone <e_m.cardone@yahoo.it>
2022-07-25 23:45:25 +0100

    Fixato click card canzone

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,8 +4,8 @@
         <div class="card-body">
             <h5 class="card-title">{{song.name}}</h5>
             <p class="card-text">{{song.artist}}</p>
-            <a href="#" class="btn btn-primary mt-2">
-                <i @click="setSong" class="fa-solid fa-play fs-4"></i>
+            <a @click="setSong" href="#" class="btn btn-primary mt-2">
+                <i  class="fa-solid fa-play fs-4 position-relative"></i>
             </a>
         </div>
     </div>
